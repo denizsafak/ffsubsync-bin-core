2b194cd57c73540f3b9dc6f5c483f5ffe17c11a2
Deniz Şafak <denizsafak98@gmail.com>
2025-01-22 16:32:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from ffsubsync.ffsubsync import main
-sys.exit(main())
+from multiprocessing import freeze_support
+if __name__ == "__main__":
+  freeze_support() # fix https://github.com/pyinstaller/pyinstaller/issues/4104
+  sys.exit(main())
